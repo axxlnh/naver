aff7552e7811445c33382697fe1fa833c3a05f29
axxlnh <ccl112459@gmail.com>
2023-05-03 19:29:10 +0900

    ver 1.0.0

---
+++ script/script.js
@@ -0,0 +1,3 @@
+//$(document).ready(function(){});
+// $(document).ready(function()제이쿼리(문서)가.준비되면(실행하겠다){};)
+$("span").css("color","hotpink"); // $(선택자). 액션(액션값); }); //end 어디가 끝인지 헷갈릴 수 있어서 마무리에 end 주석으로 꼭 작성하기
